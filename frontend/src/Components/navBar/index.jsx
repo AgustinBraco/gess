8794f036bfa0fd61a9b5d28bcbbbe687684c0c05
Totty19 <tomiexe16@gmail.com>
2024-03-19 18:59:15 -0300

    NavBar

--- frontend/src/Components/navBar/index.jsx
+++ frontend/src/Components/navBar/index.jsx
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom';
+import  Gess  from '../../Assets/Gess.png';
+import  Users  from '../../Assets/Users.png';
 
 const NavBar = () => (
 	<nav className="NvBar">
-		<Link className="link" to="/">
-			Inicio
-		</Link>
-		<Link className="link" to="/login">
-			Login
-		</Link>
-		<Link className="link" to="/registro">
-			Registro
-		</Link>
+		<div className="ContTit">
+			<img src={Gess} alt="" className='Gess'/>
+			<Link className="link TIT" to="/">Gess</Link>
+		</div>
+		<div className="ContLinks">
+			<Link className="link" to="/">
+				Home
+			</Link>
+			<Link className="link" to="/login">
+				Conocenos
+			</Link>
+			<Link className="link" to="/registro">
+				Clientes
+			</Link>
+			<Link className="link" to="/registro">
+				Servicios
+			</Link>
+			<Link className="link" to="/registro">
+				Contactanos
+			</Link>
+		</div>
+		<img src={Users} alt="" className='Users'/>
 	</nav>
 );
 
